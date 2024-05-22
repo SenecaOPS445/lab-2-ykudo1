eb4bbf97c5f425ea6e52a9bb7cd5c7e605cd12c2
ykudo1 <ykudo@myseneca.ca>
2024-05-22 10:30:49 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Author:Yuna Kudo
+#Author ID:187227210
+#Date Created:20240522
+
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
