db5b10e78493b4b77402529c5e30d5099ac266d7
ykudo1 <ykudo@myseneca.ca>
2024-05-22 10:30:21 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#Author:Yuna Kudo
+#Author ID:187227210
+#Date Created:20240522
+
+import sys
+
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
