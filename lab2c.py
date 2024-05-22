a04f05b645737490deb40d30a12d1381d1e062bb
ykudo1 <ykudo@myseneca.ca>
2024-05-22 09:03:03 -0400

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+arguments = sys.argv
+name = sys.argv[1]
+age = sys.argv[2]
+
+#name = input("Name: ")
+#age = input("Age: ")
+
+print('Hi ' + name + ', you are ' + age + ' years old.')
+
